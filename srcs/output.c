cb24458f269c965f17b25b9314b3bf4be49c4e78
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-01-02 15:58:48 -0800

    Printing symbol's value

--- srcs/output.c
+++ srcs/output.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/02 12:23:54 by pdeguing          #+#    #+#             */
-/*   Updated: 2019/01/02 13:10:17 by pdeguing         ###   ########.fr       */
+/*   Updated: 2019/01/02 15:57:53 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	print_output(int nsyms, int symoff, int stroff, void *ptr)
 	i = 0;
 	while (i < nsyms)
 	{
-		ft_printf("%s\n", strtable + nlist[i].n_un.n_strx);
+		ft_printf("%016llx %s\n", nlist[i].n_value, strtable + nlist[i].n_un.n_strx);
 		i++;
 	}
 }
